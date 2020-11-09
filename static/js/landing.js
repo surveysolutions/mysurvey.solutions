7e95608ef5da36502180286fdbf9269215b170ca
Zurab Sajaia <16375540+vavalomi@users.noreply.github.com>
2020-11-09 13:11:38 -0500

    fix the address for live stats

--- static/js/landing.js
+++ static/js/landing.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
         });
     });
 
-    $.getJSON('https://mysurvey.solutions/api/report/surveyStatistics', function (data) {
+    $.getJSON('https://pds.mysurvey.solutions/api/report/surveyStatistics', function (data) {
         $('#statistics-interviews')[0].textContent = data.interviewsCount;
         $('#statistics-surveys')[0].textContent = data.surveysCount;
         $('#statistics-countries')[0].textContent = data.countriesCount;
